8db98541e0a9909b4624b55ddb943bca1fbdd87d
Pranay Kumar <pranay.kumar@synapse.com>
2020-08-20 15:08:35 +0530

    added parameter in get named job

--- githttptriggerfunction.cs
+++ githttptriggerfunction.cs
@@ -20,14 +20,15 @@ namespace gitazurefunctionhttptrigger
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
+            string job = req.Query["job"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
-
+            
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. Good to know that you are a {job} This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
